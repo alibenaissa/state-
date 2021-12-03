05ada6d4f4a6826f998eb37ea46386e334a53e90
ali <alibenaissa565@gmail.com>
2021-12-03 17:30:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    Person: {
+      fullName: "pizza berguer",
+      bio: "checkin berguer: italien pizza",
+      imgSrc:
+        "https://lh3.googleusercontent.com/proxy/rBMwS4_NmLcKnau6kWFLT64q9YAIOrvhOxJj-J-V-OcPizOX5qiHOFvzyJzZb4vYhOrs-xf4JoAhPUPeS0zB0SxXyrqcftZfvssXBwCCiBWkpqmJ_AdGl081qvuBT4pEFjkjobe4DXK0_HwfHrIckLBMPyHT",
+      profession: "chef",
+    },
+    Activity: false,
+  };
+  handleClick = () =>
+    this.state.Activity === false
+      ? this.setState({ Activity: true })
+      : this.setState({ Activity: false });
+
+  render() {
+    if (this.state.Activity === true) {
+      return (
+        <>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              margin: "10vh auto",
+              backgroundColor: "black",
+              width: "40vw",
+              color: "white",
+              textAlign: "center",
+              borderRadius: "100%",
+            }}
+          >
+            <button onClick={this.handleClick} style={{ marginTop: "5vh" }}>
+              hide
+            </button>
+            <img
+              src={this.state.Person.imgSrc}
+              alt=""
+              style={{ width: "15vw", paddingTop: "5vh" }}
+            />
+            <p>{this.state.Person.fullName}</p>
+            <p>{this.state.Person.bio}</p>
+            <p>{this.state.Person.profession}</p>
+            <h5>{this.state.counter}</h5>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <button
+            onClick={this.handleClick}
+            style={{ margin: "20vh auto", display: "block", fontSize: "50px" }}
+          >
+            Show 
+          </button>
+        </>
+      );
+    }
+  }
 }
 
 export default App;
